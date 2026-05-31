193831d387c24feb391fab6f0438c5dd7c2b9c34
Goutami Sahu <goutamisahu05@gmail.com>
2026-05-31 18:07:04 +0530

    Add Motorcycle class that overrides Vehicle's method

---
+++ q3.kt
@@ -0,0 +1,14 @@
+open class Vehicle {
+    open fun startEngine() {
+        println("Engine started.")
+    }
+}
+class Motorcycle : Vehicle() {
+    override fun startEngine() {
+        println("Vroom!")
+    }
+}
+fun main() {
+    val myBike = Motorcycle()
+    myBike.startEngine() 
+}
